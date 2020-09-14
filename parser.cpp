5beafcd617f211aec515a23556afc252f9bcb9f8
Eliay Daisudov <eliayeliay2001@gmail.com>
2020-09-14 21:04:05 +0300

    Added, modified, changed etc...

--- parser.cpp
+++ parser.cpp
@@ -2,67 +2,75 @@
 
 pe_parser::pe_parser(LPCSTR filePath) {
 	std::ifstream file(filePath, std::ios::in | std::ios::binary | std::ios::ate);
-	std::streamsize size = file.tellg();
+	size_t size = file.tellg();
+	this->szFile = size;
 	file.seekg(0, std::ios::beg);
 
-	char* buffer = new char[READ_BYTES];
-	file.read(buffer, READ_BYTES);
+	char* buffer = new char[size];
+	file.read(buffer, size);
 	file.close();
 	this->pDOS_Header = (IMAGE_DOS_HEADER*)buffer;
 	this->pNT_Header = (IMAGE_NT_HEADERS*)(this->pDOS_Header->e_lfanew + buffer);
 	if (pNT_Header->Signature != NT_SIGNATURE) {
 		throw "Not an executable!";
 	}
+
+	// Parsing Headers
 	this->pFileHeader = (IMAGE_FILE_HEADER*)&this->pNT_Header->FileHeader;
 	this->pOptional_Header = (IMAGE_OPTIONAL_HEADER*)&pNT_Header->OptionalHeader;
-	this->pDataDirectory = (IMAGE_DATA_DIRECTORY*)&this->pOptional_Header->DataDirectory;
-	this->pDirectoryEntry_EXPORT =		(PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_EXPORT;
-	this->pDirectoryEntry_IMPORT =		(PIMAGE_DATA_DIRECTORY)&this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_IMPORT;
-	this->pDirectoryEntry_Resource =	(PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_RESOURCE;
-	this->pDirectoryEntry_Exception =	(PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_EXCEPTION;
-	this->pDirectoryEntry_Security =	(PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_SECURITY;
-	this->pDirectoryEntry_BaseReloc =	(PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_BASERELOC;
-	this->pDirectoryEntry_Debug =		(PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_DEBUG;
-	this->pDirectoryEntry_Architecture = (PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_ARCHITECTURE;
-	this->pDirectoryEntry_GlobalPtr =	(PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_GLOBALPTR;
-	this->pDirectoryEntry_TLS =			(PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_TLS;
-	this->pDirectoryEntry_Config =		(PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG;
-	this->pDirectoryEntry_BoundImport = (PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT;
-	this->pDirectoryEntry_IAT =			(PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_IAT;
-	this->pDirectoryEntry_DelayImport = (PIMAGE_DATA_DIRECTORY)this->pDataDirectory +			IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT;
-	this->pDirectoryEntry_COM_Descriptor = (PIMAGE_DATA_DIRECTORY)this->pDataDirectory +		IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR;
 	
-	int numberOfSections(getNumberOfSections());
-	this->sectionHeaders[numberOfSections];
+	// Parsing DataDirectory
+	this->pDataDirectory[0] = (IMAGE_DATA_DIRECTORY*)&this->pOptional_Header->DataDirectory;
+	for (int i = 1; i < 16; i++) {
+		this->pDataDirectory[i] = (PIMAGE_DATA_DIRECTORY)this->pDataDirectory[i-1] + 1;
+	}
+
+	// Parsing Sections
+	this->numberOfSections = (int)this->pFileHeader->NumberOfSections;
+	this->sectionHeaders[this->numberOfSections];
 	this->sectionHeaders[0] = (IMAGE_SECTION_HEADER*)((LPVOID)(this->pNT_Header + 1));
-	for (int i = 1; i < numberOfSections; i++) {
+	for (int i = 1; i < this->numberOfSections; i++) {
 		this->sectionHeaders[i] = (IMAGE_SECTION_HEADER*)(this->sectionHeaders[i-1] + 1);
 	}
+	// Parsing dataDirectories enteries
+	for (int i = 0; i < this->numberOfSections; i++) {
+		// Formula: DataDirectory[x].VirtualAddress >= Sections[i].VirtualAddress && [x].VirtualAddress <= Sections[i].VirtualAddr + sections[i].Misc.VirtualSize
+		DWORD sVA = this->sectionHeaders[i]->VirtualAddress;
+		DWORD vSz = this->sectionHeaders[i]->Misc.VirtualSize;
+		DWORD pRD = this->sectionHeaders[i]->PointerToRawData;
+		DWORD dVA = this->pDataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT]->VirtualAddress;
+		// pExport_Directory
+		if (dVA >= sVA && dVA <= sVA + vSz) { this->dataDirectories.pExport_Directory = (PIMAGE_EXPORT_DIRECTORY)(LPVOID)(dVA - sVA + pRD + buffer); }
+		// pImport
+		dVA = this->pDataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT]->VirtualAddress;
+		if (dVA >= sVA && dVA <= sVA + vSz) { this->dataDirectories.pImport_Descriptor = (PIMAGE_IMPORT_DESCRIPTOR)(LPVOID)(dVA - sVA + pRD + buffer); }
+		// pResource
+		dVA = this->pDataDirectory[IMAGE_DIRECTORY_ENTRY_RESOURCE]->VirtualAddress;
+		if (dVA >= sVA && dVA <= sVA + vSz) { this->dataDirectories.pResource_Directory = (PIMAGE_RESOURCE_DIRECTORY)(LPVOID)(dVA - sVA + pRD + this->baseAddress); }
+		// Relocation
+		dVA = this->pDataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC]->VirtualAddress;
+		if (dVA >= sVA && dVA <= sVA + vSz) { this->dataDirectories.pRelocation_Directory = (PIMAGE_BASE_RELOCATION)(LPVOID)(dVA - sVA + pRD + buffer); }
+		// Debug
+		dVA = this->pDataDirectory[IMAGE_DIRECTORY_ENTRY_DEBUG]->VirtualAddress;
+		if (dVA >= sVA && dVA <= sVA + vSz) { this->dataDirectories.pDebug_Directory = (PIMAGE_DEBUG_DIRECTORY)(LPVOID)(dVA - sVA + pRD + buffer); }
+		// TLS
+		dVA = this->pDataDirectory[IMAGE_DIRECTORY_ENTRY_TLS]->VirtualAddress;
+		if (dVA >= sVA && dVA <= sVA + vSz) { this->dataDirectories.pTLS_Directory = (PIMAGE_TLS_DIRECTORY)(LPVOID)(dVA - sVA + pRD + buffer); }
+		// Bound Import
+		dVA = this->pDataDirectory[IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT]->VirtualAddress;
+		if (dVA >= sVA && dVA <= sVA + vSz) { this->dataDirectories.pBoundImport_Descriptor = (PIMAGE_BOUND_IMPORT_DESCRIPTOR)(LPVOID)(dVA - sVA + pRD + buffer); }
+		// DelayLOAD
+		dVA = this->pDataDirectory[IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT]->VirtualAddress;
+		if (dVA >= sVA && dVA <= sVA + vSz) { this->dataDirectories.pDelayLoad_Descriptor = (PIMAGE_DELAYLOAD_DESCRIPTOR)(LPVOID)(dVA - sVA + pRD + buffer); }
+	}
+	
 }
-bool pe_parser::isDll() {
-	return((this->pFileHeader->Characteristics & IMAGE_FILE_DLL) == IMAGE_FILE_DLL) ? true : false;
-}
-bool pe_parser::is_x64() {
-	return (this->pFileHeader->Machine == MACHINE_x64) ? true : false;
-}
-bool pe_parser::isCUI() {
-	return (!pe_parser::isDll() && this->pOptional_Header->Subsystem == IMAGE_SUBSYSTEM_WINDOWS_CUI) ? (true) : (false);
-}
-bool pe_parser::isGUI() {
-	return (!pe_parser::isDll() && this->pOptional_Header->Subsystem == IMAGE_SUBSYSTEM_WINDOWS_GUI) ? (true) : (false);
-}
-bool pe_parser::isTLS_used() {
-	return (this->pDirectoryEntry_TLS->VirtualAddress == NULL) ? false : true;
-}
-int pe_parser::getNumberOfSections() {
-	return (int)this->pFileHeader->NumberOfSections;
-}
+
 
 int main(int argc, char* argv[]) {
-	if (argc == 1) return 2;
+	if (argc == 1) { return 1; }
 	pe_parser parser(argv[1]);
-	// Testing here
-
+	
 	getchar();
 	return 0;
 }
--- parser.h
+++ parser.h
@@ -5,7 +5,6 @@
 #include <fstream>
 #include <string>
 
-#define READ_BYTES				4096
 #define MZ_MAGIC				0x5A4D
 #define NT_SIGNATURE			0x4550
 #define MACHINE_x64				0x8664
@@ -15,70 +14,55 @@
 #define IMAGEBASE_DLL			0x10000000
 #define IMAGEBASE_EXE			0x00400000
 #define NUMBER_OF_DATA_DIRECTORIES 15
-
-int initiate();
-
-
 	
-class pe_parser {
-public:
+extern struct pe_parser {
 	pe_parser(LPCSTR filePath);
-	// IS THE FILE A DLL?
-	bool isDll();
-	bool is_x64();
-	// Does the program have a GUI?
-	bool isGUI();
-	// Does the program run under a commandline?
-	bool isCUI();
-	// is the TLS being used
-	bool isTLS_used();
-	// Get number of sections
-	int getNumberOfSections();
+
+	bool isDll() {
+		return((this->pFileHeader->Characteristics & IMAGE_FILE_DLL) == IMAGE_FILE_DLL) ? true : false;
+	}
+	bool is_x64() {
+		return (this->pFileHeader->Machine == MACHINE_x64) ? true : false;
+	}
+	bool isGUI() { // Does the program have a GUI?
+		return (!pe_parser::isDll() && this->pOptional_Header->Subsystem == IMAGE_SUBSYSTEM_WINDOWS_GUI) ? (true) : (false);
+	}
+	bool isCUI() { // Does the program run under a commandline?
+		return (!pe_parser::isDll() && this->pOptional_Header->Subsystem == IMAGE_SUBSYSTEM_WINDOWS_CUI) ? (true) : (false);
+
+	}
+	bool isTLS_used() { // is the TLS being used
+			return (this->pDataDirectory[IMAGE_DIRECTORY_ENTRY_TLS]->VirtualAddress == NULL) ? false : true;
+	}
+	
+
+	int numberOfSections;
+	size_t szFile;
+	LPVOID baseAddress = pDOS_Header;
 
 	// Main Headers
 	IMAGE_DOS_HEADER* pDOS_Header;
 	IMAGE_NT_HEADERS* pNT_Header;
-	IMAGE_SECTION_HEADER* pSections; // Should be an array
-
+	
 	// Parent-> pNT_Header
 	IMAGE_FILE_HEADER* pFileHeader;
 	IMAGE_OPTIONAL_HEADER* pOptional_Header;
 
 	// Parent -> optional header
-	IMAGE_DATA_DIRECTORY* pDataDirectory; // Should be array
-
-	// Parent -> DataDirectory
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_EXPORT;			// 0
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_IMPORT;			// 1
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_Resource;			// 2
-		// Parent -> Resource Directory
-		IMAGE_RESOURCE_DIRECTORY* pResource_Directory;
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_Exception;		// 3
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_Security;			// 4
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_BaseReloc;		// 5
-		// Parent -> BaseReloc
-		IMAGE_BASE_RELOCATION* pRelocation_Directory;
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_Debug;			// 6
-		// Parent -> Debug
-		IMAGE_DEBUG_DIRECTORY* pDebug_Directory;
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_Architecture;		// 7
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_Copyright;		// 7 (X86 usage)
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_GlobalPtr;		// 8
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_TLS;				// 9
-		// Parent -> TLS
-		IMAGE_TLS_DIRECTORY* pTLS_Directory;
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_Config;			// 10
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_BoundImport;		// 11
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_IAT;				// 12
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_DelayImport;		// 13
-		// Parent -> DelayImport
-		IMAGE_DELAYLOAD_DESCRIPTOR* pDelayLoad_Descriptor;
-	PIMAGE_DATA_DIRECTORY pDirectoryEntry_COM_Descriptor;	// 14
+	IMAGE_DATA_DIRECTORY* pDataDirectory[16];
 	
+	struct dataDirectory {
+		PIMAGE_EXPORT_DIRECTORY		pExport_Directory;			// Parent -> EXPORT
+		PIMAGE_IMPORT_DESCRIPTOR	pImport_Descriptor;
+		PIMAGE_RESOURCE_DIRECTORY	pResource_Directory;
+		PIMAGE_BASE_RELOCATION		pRelocation_Directory;		// Parent -> BaseReloc
+		PIMAGE_DEBUG_DIRECTORY		pDebug_Directory;			// Parent -> Debug
+		PIMAGE_TLS_DIRECTORY		pTLS_Directory;				// Parent -> TLS
+		PIMAGE_BOUND_IMPORT_DESCRIPTOR pBoundImport_Descriptor;
+		PIMAGE_DELAYLOAD_DESCRIPTOR pDelayLoad_Descriptor;		// Parent -> DelayImport
+	}; dataDirectory dataDirectories;
+
 	IMAGE_SECTION_HEADER* sectionHeaders[];
-
-
 private:
-
-
+	//void locateData(int sectionNumber, DWORD dataVirtualAddress);
 };
